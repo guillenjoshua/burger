0218aec6cfd8109c2f883bc3a8e5da26f24497d8
guillenjoshua <guillenjoshua@gmail.com>
2020-10-02 21:48:25 -0500

    setup orm, burger.js, fix controllers

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,85 @@
 const connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
 
-function selectAll() {
+  function objToSql(ob) {
+    var arr = [];
+  
+    
+    for (var key in ob) {
+      var value = ob[key];
+  
+      if (Object.hasOwnProperty.call(ob, key)) {
+      
+        
+       
+        arr.push(key + "=" + value);
+      }
+    }
+  
+  
+    return arr.toString();
+  }
+  
 
-
-}
-
-
-function insertOne() {
-
-
-}
-
-
-
-
-function updateOne() {
-
-
-}
+  var orm = {
+    SelectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+  };
 
 
 module.exports = orm;
