ddc8ab05cd4d3dcc1b79b353d55aee475a5b3e8a
guillenjoshua <guillenjoshua@gmail.com>
2020-10-11 10:17:47 -0500

    Adding JAWSDB

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,50 +1,5 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function() {
-  //   $(".devoured").on("click", function(event) {
-  //     var id = $(this).data("id");
-  //     if ($(this).data("devoured") === 0){
-  //       var devoured = true
-  //    }else devoured = false
-  
-  //    console.log(devoured);
-  //    console.log(id);
-  //    var newDevoured = {
-  //        devoured: devoured
-  //    };
-        
-  //     // Send the PUT request.
-  //     $.ajax("/api/burgers/" + id, {
-  //       type: "PUT",
-  //       data: newDevoured
-  //     }).then(
-  //       function() {
-  //         console.log("changed devoured to", devoured);
-  //         // Reload the page to get the updated list
-  //         location.reload();
-  //       }
-  //     );
-  //   });
-  
-  // //   $(".create-form").on("submit", function(event) {
-  //     // Make sure to preventDefault on a submit event.
-  //     event.preventDefault();
-  
-  //     var newBurger = {
-  //       burger_name: $("#burg").val().trim(),
-  //       devoured: $("[name=devoured]:checked").val().trim()
-  //     };
-  
-  //     // Send the POST request.
-  //     $.ajax("/api/burgers", {
-  //       type: "POST",
-  //       data: newBurger
-  //     }).then(
-  //       function() {
-  //         console.log("created new Burger!");
-  //         // Reload the page to get the updated list
-  //         location.reload();
-  //       }
-  //     );
-  //   });
+ 
  });
   
